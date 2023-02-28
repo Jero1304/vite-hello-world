519f88a51fa62a911281a53c30aaa064af7e99f8
Jero1304 <calogeroleto13@gmail.com>
2023-02-28 16:29:48 +0100

    FEAT message

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default { 
+  data(){
+    return{
+      message:'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+
+  <div class="text">
+    <h1>{{ message }}</h1>
+  </div>
+
+</template>
+
+<style scoped>
+.text{
+  color: red;
+  background-color: gold;
+  border-radius: 20px ;
+  padding: 20px;
+}
+</style>
